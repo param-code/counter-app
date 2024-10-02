db21f74b39d16dc98700165d5014c3f85e229fdb
abishek1740P <abisheksivarajan@gmail.com>
2024-10-02 13:44:19 +0530

    feat : Added Timer Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import AutoCounterPage from './pages/AutoCounterPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AutoCounterPage from "./pages/AutoCounterPage"; // adjust the path as needed
+import TimerPage from "./pages/TimerPage"; // create this component
 
 const App = () => {
   return (
-      <AutoCounterPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<AutoCounterPage />} />
+        <Route path="/timer" element={<TimerPage />} />
+      </Routes>
+    </Router>
   );
 };
 
