c60f07639374deb2583e64d4f78de65d98f8feb0
Suvadip-sana <suvadipsana602@gmail.com>
2024-10-04 00:20:21 +0530

    Add button of timer & world clock with cool animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,9 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Button } from "./ui/button"
 import { Github, Moon, Sun, Globe } from "lucide-react"
 import logo from "../assets/logo.png"
+import timer from "../assets/timer1.png";
+import worldClock from "../assets/clock.png";
+import "./css/navbar.css"
 
 const Navbar = ({ theme, toggleTheme }) => {
   const navigate = useNavigate();
@@ -10,11 +13,25 @@ const Navbar = ({ theme, toggleTheme }) => {
     <nav className="bg-white dark:bg-gray-800 shadow-md transition-colors duration-300 sticky top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <Link to="/" className="font-bold text-xl text-blue-600 dark:text-blue-400">
+          <div className="butt flex items-center">
+            <Link to="/" className="page font-bold text-xl text-blue-600 dark:text-blue-400 mr-8">
               <img
                 src={logo}
                 alt="Counter"
+                className="h-9 w-55"
+              />
+            </Link>
+            <Link to="/timer" className="pages font-bold text-xl text-blue-600 dark:text-blue-400 mr-8">
+              <img
+                src={timer}
+                alt="Timer"
+                className="h-9 w-55"
+              />
+            </Link>
+            <Link to="/world-clock" className="pages font-bold text-xl text-blue-600 dark:text-blue-400">
+              <img
+                src={worldClock}
+                alt="World Clock"
                 className="h-9 w-55"
               />
             </Link>
@@ -31,14 +48,14 @@ const Navbar = ({ theme, toggleTheme }) => {
               </Button>
 
               {/* World Clock button */}
-              <Button
+              {/* <Button
                 onClick={() => navigate('/world-clock')}
                 variant="ghost"
                 size="icon"
                 className="rounded-full text-gray-600 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 mr-2"
               >
                 <Globe className="h-5 w-5" />
-              </Button>
+              </Button> */}
 
               <Button
                 onClick={toggleTheme}
