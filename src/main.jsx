6e03a9699b563d43dc068a8ca4502938f5010e57
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-12 10:13:07 +0530

    Toast feature added for notification

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 )
