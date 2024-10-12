040045580ee03b5825e03da93ad33463f1616372
P Sreyanshu Anupam <p.sreyanshuspazz@gmail.com>
2024-10-12 08:27:54 +0530

    added background blur in dark theme for the box in world clock page.

--- src/pages/WorldClockPage.jsx
+++ src/pages/WorldClockPage.jsx
@@ -160,7 +160,7 @@ const WorldClockPage = () => {
       )}
       <Navbar theme={theme} toggleTheme={toggleTheme} />
       <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)] p-4 sm:p-6 md:p-8">
-        <div className="bg-opacity-10 backdrop-blur-sm bg-slate-400 dark:bg-gray-800 rounded-lg shadow-xl p-6 sm:p-8 w-full max-w-sm sm:max-w-md md:max-w-lg transition-all duration-300 ease-in-out">
+        <div className="bg-opacity-10 backdrop-blur-sm bg-slate-400 dark:bg-gray-800 dark:bg-opacity-10 dark:backdrop-blur-sm rounded-lg shadow-xl p-6 sm:p-8 w-full max-w-sm sm:max-w-md md:max-w-lg transition-all duration-300 ease-in-out">
           <h1 className="text-3xl font-bold mb-6 text-center text-gray-800 dark:text-white">
             World Clock
           </h1>
