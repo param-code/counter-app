d6828ff283171672dda51b612212be8f4fbcdb44
Paramveer Singh <109423225+param-code@users.noreply.github.com>
2024-10-13 22:41:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
 import WorldClockPage from './pages/WorldClockPage';
 // import SignUpPage from "./pages/SignUpPage";
 import TimerPage from './pages/TimerPage';
