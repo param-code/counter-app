91cf917f90112b6c41a71753aac2c336b0e4bbd4
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-10-24 00:46:16 +0530

    Enhanced the ui of login and signup pages

--- src/components/Auth/SignupForm.jsx
+++ src/components/Auth/SignupForm.jsx
@@ -77,136 +77,136 @@ function SignupForm() {
   };
 
   return (
-    <div className='py-2'>
+    <div className="py-2">
       {/* Form */}
       <form
         noValidate
         onSubmit={handleOnSubmit}
-        className='flex w-full font-semibold flex-col gap-y-6 text-black border-[2px] border-black p-6 rounded-md'
+        className="flex w-full font-semibold flex-col gap-y-6 text-black p-6 rounded-2xl shadow-blue-200 shadow-xl"
       >
-        <div className='flex gap-x-4'>
+        <div className="flex gap-x-4">
           <label>
-            <p className='mb-1 text-[0.95rem] leading-[1.375rem]'>First Name</p>
+            <p className="mb-1 text-[0.95rem] leading-[1.375rem]">First Name</p>
             <input
               required
-              type='text'
-              name='firstName'
+              type="text"
+              name="firstName"
               value={firstName}
               onChange={handleOnChange}
-              placeholder='Enter first name'
-              className='form-style w-full  p-2 border-[1px] bg-white border-black rounded-md  text-black'
+              placeholder="Enter first name"
+              className="form-style w-full p-2 shadow-md bg-white rounded-lg  text-black border-[1px] border-transparent hover:border-blue-500 hover:border-[1px]"
             />
             {errors.firstName && (
-              <div className='text-red-600'>{errors.firstName}</div>
+              <div className="text-red-600">{errors.firstName}</div>
             )}
           </label>
           <label>
-            <p className='mb-1 text-[0.95rem] leading-[1.375rem] text-richblack-5'>
+            <p className="mb-1 text-[0.95rem] leading-[1.375rem] text-richblack-5">
               Last Name
             </p>
             <input
               required
-              type='text'
-              name='lastName'
+              type="text"
+              name="lastName"
               value={lastName}
               onChange={handleOnChange}
-              placeholder='Enter last name'
-              className='form-style p-2 border-[1px] border-black bg-white rounded-md w-full text-black'
+              placeholder="Enter last name"
+              className="form-style p-2 shadow-md  bg-white rounded-lg w-full text-black border-[1px] border-transparent hover:border-blue-500 hover:border-[1px]"
             />
             {errors.lastName && (
-              <div className='text-red-600'>{errors.lastName}</div>
+              <div className="text-red-600">{errors.lastName}</div>
             )}
           </label>
         </div>
-        <label className='w-full'>
-          <p className='mb-1 text-[0.95rem] leading-[1.375rem] '>
+        <label className="w-full">
+          <p className="mb-1 text-[0.95rem] leading-[1.375rem] ">
             Email Address
           </p>
           <input
             required
-            type='text'
-            name='email'
+            type="text"
+            name="email"
             value={email}
             onChange={handleOnChange}
-            placeholder='Enter email address'
-            className='form-style w-[96%]  p-2 border-[1px] bg-white border-black rounded-md  text-black'
+            placeholder="Enter email address"
+            className="form-style w-full p-2 shadow-md bg-white rounded-lg  text-black border-[1px] border-transparent hover:border-blue-500 hover:border-[1px]"
           />
-          {errors.email && <div className='text-red-600'>{errors.email}</div>}
+          {errors.email && <div className="text-red-600">{errors.email}</div>}
         </label>
-        <div className='flex gap-x-4'>
-          <label className='relative'>
-            <p className='mb-1 text-[0.95rem] leading-[1.375rem] text-richblack-5'>
+        <div className="flex gap-x-4">
+          <label className="relative">
+            <p className="mb-1 text-[0.95rem] leading-[1.375rem] text-richblack-5">
               Create Password
             </p>
             <input
               required
               type={showPassword ? "text" : "password"}
-              name='password'
+              name="password"
               value={password}
               onChange={handleOnChange}
-              placeholder='Enter Password'
-              className='form-style w-full !pr-10  p-2 border-[1px] bg-white border-black rounded-md  text-black'
+              placeholder="Enter Password"
+              className="form-style w-full !pr-10  p-2 shadow-md bg-white rounded-lg  text-black border-[1px] border-transparent hover:border-blue-500 hover:border-[1px]"
             />
             <span
               onClick={() => setShowPassword((prev) => !prev)}
-              className='absolute right-3 top-[35px] z-[10] cursor-pointer'
+              className="absolute right-3 top-[35px] z-[10] cursor-pointer"
             >
               {showPassword ? (
-                <AiOutlineEyeInvisible fontSize={24} fill='#000000' />
+                <AiOutlineEyeInvisible fontSize={24} fill="#000000" />
               ) : (
-                <AiOutlineEye fontSize={24} fill='#000000' />
+                <AiOutlineEye fontSize={24} fill="#000000" />
               )}
             </span>
             {errors.password && (
-              <div className='text-red-600'>{errors.password}</div>
+              <div className="text-red-600">{errors.password}</div>
             )}
           </label>
-          <label className='relative'>
-            <p className='mb-1 text-[0.95rem] leading-[1.375rem] text-richblack-5'>
+          <label className="relative">
+            <p className="mb-1 text-[0.95rem] leading-[1.375rem] text-richblack-5">
               Confirm Password
             </p>
             <input
               required
               type={showConfirmPassword ? "text" : "password"}
-              name='confirmPassword'
+              name="confirmPassword"
               value={confirmPassword}
               onChange={handleOnChange}
-              placeholder='Confirm Password'
-              className='form-style w-full !pr-10 border-black  p-2 border-[1px] bg-white rounded-md  text-black'
+              placeholder="Confirm Password"
+              className="form-style w-full !pr-10 shadow-md  p-2 bg-white rounded-lg  text-black border-[1px] border-transparent hover:border-blue-500 hover:border-[1px]"
             />
             <span
               onClick={() => setShowConfirmPassword((prev) => !prev)}
-              className='absolute right-3 top-[35px] z-[10] cursor-pointer text-black'
+              className="absolute right-3 top-[35px] z-[10] cursor-pointer text-black"
             >
               {showConfirmPassword ? (
-                <AiOutlineEyeInvisible fontSize={24} fill='#000000' />
+                <AiOutlineEyeInvisible fontSize={24} fill="#000000" />
               ) : (
-                <AiOutlineEye fontSize={24} fill='#000000' />
+                <AiOutlineEye fontSize={24} fill="#000000" />
               )}
             </span>
             {errors.confirmPassword && (
-              <div className='text-red-600'>{errors.confirmPassword}</div>
+              <div className="text-red-600">{errors.confirmPassword}</div>
             )}
           </label>
         </div>
         <button
-          type='submit'
-          className='mt-6 font-semibold py-[8px] px-[12px] text-white p-2 border-[1px] rounded-md  bg-red-600 hover:bg-red-700 border-red-800'
+          type="submit"
+          className="mt-6 font-semibold py-[8px] px-[12px] text-white p-2 border-[1px] rounded-lg  bg-red-600 hover:bg-red-700 border-red-800"
         >
           Create Account
         </button>
-        <div className='flex gap-2 flex-col mt-2'>
-          <div className='flex gap-2 justify-center items-center'>
-            <span className='h-[1px] bg-black w-[70%]'></span>
+        <div className="flex gap-2 flex-col mt-2">
+          <div className="flex gap-2 justify-center items-center">
+            <span className="h-[1px] bg-black w-[70%]"></span>
             <span>OR</span>
-            <span className='h-[1px] bg-black w-[70%]'></span>
+            <span className="h-[1px] bg-black w-[70%]"></span>
           </div>
-          <div className='flex flex-row mx-auto gap-4 justify-center items-center'>
-            <div className=' font-normal text-black select-none'>
-              Have an account?
+          <div className="flex flex-row mx-auto gap-4 justify-center items-center">
+            <div className=" font-normal text-black select-none">
+              Already have an account?
             </div>
             <button
-              className='text-blue-700 hover:text-blue-950 font-semibold cursor-pointer'
+              className="text-blue-700 hover:text-blue-950 font-semibold cursor-pointer"
               onClick={() => navigate("/login")}
             >
               Sign in
