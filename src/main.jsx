a9e12f4b748df102b00a7583524679bbf370dc91
amiya <amiyasingh56@gmail.com>
2024-10-28 18:26:26 +0530

    githubworkflow

--- src/main.jsx
+++ src/main.jsx
@@ -22,3 +22,21 @@ createRoot(document.getElementById("root")).render(
 	</StrictMode>
 );
 
+const scrollBtn = document.querySelector('.scroll-btn') ;
+
+
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = 'block' ;
+    }
+    else {
+        scrollBtn.style.display = 'none' ;
+    }
+})
+scrollBtn.addEventListener('click' , () => {
+    window.scroll({
+        top: 0 ,
+        behavior: "smooth"
+    })
+})
+
